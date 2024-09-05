96a1187e692d312d44d001b07dbb874f4a80b6a7
Wally1403 <christobaudouin@gmail.com>
2024-09-05 17:09:34 +0200

    start routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/'
+import Survey from './pages/Survey'
+import Header from './components/Header'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.render(
     <React.StrictMode>
-        <Home />
-    </React.StrictMode>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/survey" element={<Survey />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
